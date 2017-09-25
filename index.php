49311b2c68dad32b5fc98360f7c93b1a1fd6a5e1
julesguillemot <julesguillemot@sfr.fr>
2017-09-25 15:54:10 +0200

    4e version: application des posts

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
  ?>
  <section>
    <h3>Section centrale</h3>
-   <h3>Sans titre<h3> 
+   <h3>Sans titre<h3>
+     <!--The loop-->
+     <?php if(have_posts()):while(have_posts()):
+       the_post(); ?>
+       <h3><?php the_title(); ?></h3>
+       <p><?php the_content(); ?></p>
+     <?php endwhile;endif; ?>
  </section>
 <?php
   include ('footer.php');
