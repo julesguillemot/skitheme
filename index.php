887077f4ff21c9a6cc9a7628aba4c9eb811edf38
julesguillemot <julesguillemot@sfr.fr>
2017-09-25 15:59:30 +0200

    Mise en place des articles

--- index.php
+++ index.php
@@ -4,16 +4,13 @@
  <section>
    <h3>Section centrale</h3>
    <h3>Sans titre<h3>
-<<<<<<< HEAD
-     <!--The loop-->
-     <?php if(have_posts()):while(have_posts()):
-       the_post(); ?>
-       <h3><?php the_title(); ?></h3>
-       <p><?php the_content(); ?></p>
-     <?php endwhile;endif; ?>
-=======
->>>>>>> 19c652921b159a3e401c24f7f8b5f5afc7c72f67
  </section>
+ <!--The loop-->
+ <?php if(have_posts()):while(have_posts()):
+   the_post(); ?>
+   <h3><?php the_title(); ?></h3>
+   <p><?php the_content(); ?></p>
+ <?php endwhile;endif; ?> 
 <?php
   get_footer();
  ?>
