79e1f5e94acebe4dbdad11c355c7a3c41d0f13b3
julesguillemot <julesguillemot@sfr.fr>
2017-09-25 16:00:46 +0200

    application sidebar

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
    the_post(); ?>
    <h3><?php the_title(); ?></h3>
    <p><?php the_content(); ?></p>
- <?php endwhile;endif; ?> 
+ <?php endwhile;endif; ?>
+ <?php get_sidebar(); ?>
 <?php
   get_footer();
  ?>
